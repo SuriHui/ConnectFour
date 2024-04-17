9e6337aa1e8378b845f85ff3bb24b1333976de88
Suri Hui <suri.hui@mrjaffesclass.com>
2024-04-17 11:42:06 -0700

    siwtch players and make eventLoop

--- ConnectFour-copy/ConnectFour.java
+++ ConnectFour-copy/ConnectFour.java
@@ -4,25 +4,32 @@ public class ConnectFour
     static Scanner scr = new Scanner(System.in);
     private Player p1;
     private Player p2;
+    private Player currentPlayer;
     static char[][] board = new char[Constants.BOARD_HEIGHT][Constants.BOARD_LENGTH];
-    
-    public ConnectFour() {
-        makeBoard();
-        printBoard();
-        
+
+    public void main(String arg[]) {
+        ConnectFour eventLoop = new ConnectFour();
+        eventLoop.run();
     }
-    
-    public static void main(String arg[]) {
-        System.out.println("Who is PLayer 1?");
-        String p1 = scr.next();
-        Player player1 = new Player(p1, Constants.P1_COLOR);
-        System.out.println("Player 1: " + player1.toString());
-        System.out.println("\nPlayer 2?");
-        String p2 = scr.next();
-        Player player2 = new Player(p2, Constants.P2_COLOR);
-        System.out.println("Player 2: " + player2.toString());
+
+    public void run() {
+        boolean shouldContinue = true;
+        while (shouldContinue == true) {
+            System.out.println("Who is PLayer 1?");
+            String p1 = scr.next();
+            Player player1 = new Player(p1, Constants.P1_COLOR);
+            System.out.println("Player 1: " + player1.toString());
+            System.out.println("\nPlayer 2?");
+            String p2 = scr.next();
+            Player player2 = new Player(p2, Constants.P2_COLOR);
+            System.out.println("Player 2: " + player2.toString());
+            if (shouldContinue) {
+                
+            }
+        }
+        System.out.println("\nThank you for playing Connect Four!");
     }
-    
+
     public static void makeBoard() {
         for (int row = 0; row < Constants.BOARD_HEIGHT; row++) {
             for (int col = 0; col < Constants.BOARD_LENGTH; col++) {
@@ -31,6 +38,10 @@ public class ConnectFour
         }
     }
     
+    public int getBoard(int row, int col) {
+        return this.board[row][col];
+    }
+
     public void printBoard() {
         for (int row = 0; row < Constants.BOARD_HEIGHT; row++) {
             for (int col = 0; col < Constants.BOARD_LENGTH; col++) {
@@ -38,6 +49,22 @@ public class ConnectFour
             }
             System.out.println();
         }
-        System.out.println();
     }
+    
+    //https://stackoverflow.com/questions/46854093/switch-players-in-a-java-game
+    public String turns() {
+        
+    }
+    
+    //My teacher's base code
+     public boolean isTie() {
+        for (int row=0; row<Constants.BOARD_HEIGHT; row++) {
+            for (int col=0; col<Constants.BOARD_LENGTH; col++) {
+                if (getBoard(row,col) == Constants.EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    } 
 }
