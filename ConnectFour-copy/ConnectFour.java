0024ad646769248ecd97b38bec020e34fc225742
Suri Hui <suri.hui@mrjaffesclass.com>
2024-04-19 11:41:48 -0700

    test code, make winner

--- ConnectFour-copy/ConnectFour.java
+++ ConnectFour-copy/ConnectFour.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 public class ConnectFour
 {
     static Scanner scr = new Scanner(System.in);
     private Player p1;
     private Player p2;
-    private Player currentPlayer;
     static char[][] board = new char[Constants.BOARD_HEIGHT][Constants.BOARD_LENGTH];
 
     //https://codereview.stackexchange.com/questions/100917/connect-four-game-in-java (use as reference)
@@ -14,6 +14,8 @@ public class ConnectFour
     }
 
     public void run() {
+        makeBoard();
+        System.out.println("Welcome to Connect Four!");
         boolean shouldContinue = true;
         while (shouldContinue == true) {
             System.out.println("Who is PLayer 1?");
@@ -24,9 +26,8 @@ public class ConnectFour
             String p2 = scr.next();
             Player player2 = new Player(p2, Constants.P2_COLOR);
             System.out.println("Player 2: " + player2.toString());
-            if (shouldContinue) {
-                
-            }
+            System.out.println("Select 1-6 to choose what column you want");
+            printBoard();
         }
         System.out.println("\nThank you for playing Connect Four!");
     }
@@ -52,14 +53,9 @@ public class ConnectFour
         }
     }
     
-    //https://stackoverflow.com/questions/46854093/switch-players-in-a-java-game
-    public String turns() {
-        
-    }
-    
     //https://stackoverflow.com/questions/32770321/connect-4-check-for-a-win-algorithm 
     public boolean isWin() {
-
+        
     }
     
     //My teacher's base code
@@ -73,4 +69,50 @@ public class ConnectFour
         }
         return true;
     } 
+    
+    //teacher's code base
+    public boolean isLegalMove(int row, int col) {
+        if (1 <= col && col <= Constants.BOARD_LENGTH &&
+            getBoard(row-1, col- 1) == Constants.EMPTY) {
+            return true;
+        } else {
+            System.out.println("Invalid column");
+            return false;
+        } 
+    }
+    
+    //teachers base code
+    public String getROrY(int whoseMove) {
+        if (whoseMove == -1) {
+            return "R";
+        } else if (whoseMove == 1) {
+            return "Y";
+        } else {
+            return " ";
+        }
+    }
+    
+    //teachers base code
+    public int getMoveCol (int whoseMove) {
+        int col = 0;
+        while (true) {
+            System.out.printf(getROrY(whoseMove));
+            try {
+                col= scr.nextInt();
+                if (col < 1 || col > Constants.BOARD_LENGTH) {
+                    System.out.println("Invalid Column");
+                    System.out.println();
+                    scr.nextLine();
+                }
+                else {
+                    return col;
+                }
+            } catch (InputMismatchException error) {
+                System.out.println("Invalid Column");
+                System.out.println();
+                scr.next();
+            }
+        }   
+    }
+    
 }
